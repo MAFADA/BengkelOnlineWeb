d0c7c8363817331ff3186b8c8a0694e1111b7fb1
AFADA <afadanursaiva2001@gmail.com>
2021-12-11 07:28:57 +0700

    First Setup

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,13 +15,15 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->nullable();
             $table->string('username',20)->nullable()->unique();
-            $table->string('name');
             $table->string('email')->unique()->nullable();
+            $table->string('photo')->nullable();            
             $table->string('role')->nullable();
             $table->string('phone_number')->nullable();
+            $table->string('address')->nullable();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password');            
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/migrations/2021_12_10_235841_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('category_id')->nullable();
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->string('product_name')->nullable();
+            $table->integer('stock')->nullable();
+            $table->integer('price')->nullable();
+            $table->string('photo')->nullable();
+            $table->string('description')->nullable();            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
