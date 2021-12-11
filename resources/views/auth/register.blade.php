5224d89330ba2a7d01adf4012c7494495c800db3
AFADA <afadanursaiva2001@gmail.com>
2021-12-11 07:46:18 +0700

    Setup2

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -75,6 +75,16 @@
                             </div>
                         </div>
 
+                        <div class="form-group row">
+                            <label for="role" class="col-md-4 col-form-label text-md-right">{{ __('Role') }}</label>
+                            <div class="col-md-6">
+                                <select class="form-control" name="role">
+                                <option value="customer">Customer</option>
+                                <option value="admin">Admin</option>
+                            </select>
+                            </div>
+                        </div>
+
                         <div class="row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
