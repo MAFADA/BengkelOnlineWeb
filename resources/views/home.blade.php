ad6971bb1c5bb34c91b30deef330a90e4debdba3
AFADA <afadanursaiva2001@gmail.com>
2021-12-11 09:15:32 +0700

    halaman home done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,14 +3,18 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        @foreach($products)
+        @foreach($products as $p)
         <div class="col-md-4">
-            <div class="card" style="width: 18rem;">
+            <div class="card" >
                 <img class="card-img-top" src="..." alt="Card image cap">
                 <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                    <h5 class="card-title">{{$p->product_name}}</h5>
+                    <p class="card-text">
+                        <strong>Harga   :</strong> Rp. {{number_format($p->price)}} <br>
+                        <!-- <strong>Stock   :</strong> {{$p->stock}}<br> -->
+                        <!-- {{$p->description}} -->
+                    </p>
+                    <a href="#" class="btn btn-primary"><i class="fa fa-eye"></i> Detail</a>
                 </div>
             </div>
         </div>        
